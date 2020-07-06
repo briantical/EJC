99c1a0f3c42fefd17756c0b11e790914826340af
briantical <lutbrianivan@gmail.com>
2020-07-06 10:10:21 +0300

    Added a home route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,9 @@ const { insertUser, fetchUser } = require('./handlers');
 
 const routes = Router();
 
+routes.get('/', (req, res) => {
+  res.json({ message: 'Welcome to The API Server' });
+});
 routes.post('/user', insertUser);
 routes.get('/user/:username', fetchUser);
 
