7ccb74ab3d275dc934cad6edecc8dd9afb3a83d9
briantical <lutbrianivan@gmail.com>
2020-07-06 23:42:30 +0300

    Heroku port

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 const app = require('./src/index');
 
-const PORT = process.env.port;
 const DB_URI = 'mongodb://briantical:lutbrian96IV*@ds255958.mlab.com:55958/heroku_ln8xkrgn';
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true
 };
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Server started on ${PORT}`);
 });
 
