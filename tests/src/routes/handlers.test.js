07dc823c18dbcd3b2967b617d813a95bd3ee942d
briantical <lutbrianivan@gmail.com>
2020-07-06 15:50:56 +0300

    Configuring mongodb image

--- tests/src/routes/handlers.test.js
+++ tests/src/routes/handlers.test.js
@@ -24,13 +24,12 @@ const mockResponse = () => {
 };
 
 describe('Test User route handlers', () => {
-  beforeAll(async (done) => {
+  beforeAll(async () => {
     try {
       await mongoose.connect(DB_URI, options);
       // console.log('Database (TEST) is now connected');
     } catch (error) {
       console.log('Database connection error');
-      done();
     }
   });
 
